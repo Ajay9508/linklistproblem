b24284de966da2d1f1407ae73dd37a536d3c65d1
Ajay kumar <121481541+Ajay9508@users.noreply.github.com>
2026-02-22 18:43:56 +0530

    Add files via upload
    take array :12 45 67 23 88 44 into linklist  traverse same as it. 12 45 67 23 88 44.

---
+++ arrayoflinklistback.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node*next;
+    node(int value){
+        data = value;
+        next = NULL;
+    }
+};
+int main(){
+    node  *head,*tail;
+    head = tail= NULL;
+    int arr[6]={12,45,67,23,88,44};
+    for(int i=0;i<6;i++){
+        if(head == NULL){
+            head = new node(arr[i]);
+            tail = head;
+        }else{
+            tail ->next = new node(arr[i]);
+            tail  = tail -> next;
+        }
+    }
+    while (head)
+    {
+    cout<<head-> data << " ";
+    head = head -> next;
+    }
+    cout << endl;
+    return 0;
+}
